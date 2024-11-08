e0a12a746b2685f1b39bd5aa1abc844fe87a26ab
tahrel <tahrel44@gmail.com>
2024-11-08 13:03:11 +0100

    update

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,52 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+
+
+arrowLeft.addEventListener('click', function() {
+    console.log('Clic sur la flèche gauche');
+    // alert('la flèche gauche');
+    
+});
+
+
+arrowRight.addEventListener('click', function() {
+    console.log('Clic sur la flèche droite');
+    // alert('la flèche droite');
+  
+});
+
+
+const bannerImg = document.querySelector('.banner-img'); 
+const tagLine = document.querySelector('#banner p');
+const dots = document.querySelectorAll('.dot');
+let currentIndex = 0; 
+
+
+function updateSlide(index) {
+	
+	bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
+	
+	tagLine.innerHTML = slides[index].tagLine;
+
+	dots.forEach((dot, i) => {
+		dot.classList.toggle('dot_selected', i === index);
+	});
+}
+
+document.querySelector('.arrow_right').addEventListener('click', () => {
+	currentIndex = (currentIndex + 1) % slides.length; 
+	updateSlide(currentIndex);
+});
+
+
+document.querySelector('.arrow_left').addEventListener('click', () => {
+	currentIndex = (currentIndex - 1 + slides.length) % slides.length; 
+	updateSlide(currentIndex);
+});
+
+
+updateSlide(currentIndex);
